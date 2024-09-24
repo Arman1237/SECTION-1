0f333509e0ce56ce4a8ba293cedffebeb640c093
Arman1237 <nasirkhan780022@gmail.com>
2024-09-24 17:30:01 +0530

    conditions & loops

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('Hii java');
+ 
+a=234;
+
+console.log(a);
+console.log( typeof a);
+
+message = "Be Calm";
+
+console.log(message);
+console.log(typeof message);
+
+isHungry = true;
+console.log(isHungry);
+
+[x, y, z] = [2, 3, 4];
+
+console.log(x+y+z);
+
+// exponent operator
+console.log(2**4);
+
+console.log( 1+1 );
+console.log( 1+'1' );
+console.log( 1+1+'1' );
+console.log( 1+'1'+1 );
+console.log( null+'1');
+console.log( 1 == 1 );
+console.log( 1 == '1');
+console.log( 1 === '1');
+
---
+++ loops.js
@@ -0,0 +1,22 @@
+// for, while, do-while
+
+for(let i=0; i<10; i++){
+    console.log(i);
+    
+}
+
+let a = 10;
+
+while (a<20){
+    console.log(a);
+    a+=2;
+    
+}
+console.log('------------------------');
+
+let b = 20
+do{
+    console.log(b);
+    a+=2;
+    
+}while(b<20);
